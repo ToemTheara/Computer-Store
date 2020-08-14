488ebe1078f321f1618bae849fe9752cab615a7f
Toem Theara <thearatoem11@gmail.com>
2020-08-14 23:17:43 +0700

    Update Navigationbar and Footer

--- resources/views/web_user/layouts/footer.blade.php
+++ resources/views/web_user/layouts/footer.blade.php
@@ -1,11 +1,56 @@
 <!-- Footer -->
-<footer class="py-5 bg-dark">
-  <div class="container">
-    <p class="m-0 text-center text-white">Thanks You!</p>
-  </div>
-  <!-- /.container -->
-</footer>
 
+<!-- Footer -->
+<footer class="text-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-lg-4 col-xl-3">
+                <h5>About</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <p class="mb-0">
+                    Computer Store Co., Ltd was founded on 7th of January 2001 as a small second-hand computer retailer. At the beginning, we sold
+                    refurbished desktops and laptops which we imported from Japan and USA to our local customers.
+                </p>
+            </div>
+
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto">
+                <h5>Our Products</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="list-unstyled">
+                    <li><a href="/msi">MSI Computer</a></li>
+                    <li><a href="/apple">APPLE Computer</a></li>
+                    <li><a href="/dell">DELL Computer</a></li>
+                    <li><a href="/lenovo">LENOVO Computer</a></li>
+                    <li><a href="/asus">ASUS Computer</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+                <h5>Our Address</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="list-unstyled">
+                    <li><a href="https://www.google.com/maps/dir/?api=1&destination=11.568062557%2C104.910444429&fbclid=IwAR0nFKDFDDimsy3IFauOVjLFYoNzylmaG7Vnl-O9Vdd3hYVBlWicf7jRxwI">Address: No. 360-362Eo, Kampuchea Krom Blvd., Sangkat Mittapheap, Khan 7 Makara, Phnom Penh, Cambodia</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 col-lg-3 col-xl-3">
+                <h5>Contact</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-home mr-2"></i> My company</li>
+                    <li><i class="fa fa-envelope mr-2"></i> thearatoem11@gmail.com</li>
+                    <li><i class="fa fa-phone mr-2"></i> 0966 622 798</li>
+                    <li><i class="fa fa-print mr-2"></i> 067 607 287</li>
+                </ul>
+            </div>
+            <div class="col-12 copyright mt-3">
+                <p class="float-left">
+                    <a href="#">Back to top</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
 <!-- Bootstrap core JavaScript -->
 <script src="{{asset('templates/vendor/jquery/jquery.min.js')}}"></script>
 <script src="{{asset('templates/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
--- resources/views/web_user/layouts/nav_bar.blade.php
+++ resources/views/web_user/layouts/nav_bar.blade.php
@@ -1,14 +1,13 @@
-
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container">
-            <a class="navbar-brand" href="#">COMPUTER STORE</a>
+            <a class="navbar-brand" href="/">COMPUTER STORE </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item ">
+                    {{--  <li class="nav-item ">
                         <a class="nav-link" href="/">Home</a>
                     </li>
                     <li class="nav-item">
@@ -19,9 +18,22 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contact</a>
-                    </li>
+                    </li>  --}}
                     <li class="nav-item">
-                        <a class="nav-link" href="cart.html">Cart</a>
+                        <form class="form-inline my-2 my-lg-0">
+                            <div class="input-group input-group-sm">
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Search for computer...">
+                                <div class="input-group-append">
+                                    <button type="button" class="btn btn-secondary btn-number">
+                                        <i class="fa fa-search">Search</i>
+                                    </button>
+                                </div>
+                            </div>
+                            {{--  <a class="btn btn-outline-light btn-sm ml-3" href="cart.html">
+                                <i class="fa fa-shopping-cart"></i> Cart
+                                <span class="badge badge-light">3</span>
+                            </a>  --}}
+                        </form>
                     </li>
                 </ul>
             </div>
